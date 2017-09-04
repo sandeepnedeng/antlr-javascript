2f53315863ff2e0881e8f6bfd4f6578098eb4013
sandeepn <sandeepnagaraj@Sandeeps-MacBook-Pro.local>
2017-09-04 18:00:27 +0100

    Adding Validation

--- ErrorHandlingJS/src/index.js
+++ ErrorHandlingJS/src/index.js
@@ -1,49 +1,36 @@
 
 
 import * as antlr4 from "antlr4";
-
 import * as Tree from "antlr4/tree/Tree";
+import * as Error from "antlr4/error/ErrorListener";
 
 import ArrayInitLexer from './languageDefinition/ArrayInitLexer';
 import ArrayInitParser from './languageDefinition/ArrayInitParser';
 import ArrayInitListener from './languageDefinition/ArrayInitListener';
 
-var input = "{1, 2, 3}";
+var input = "{1, 2, fkjdfsdjf}";
+
 var chars = new antlr4.InputStream(input);
 var lexer = new ArrayInitLexer.ArrayInitLexer(chars);
 var tokens = new antlr4.CommonTokenStream(lexer);
-
 var parser = new ArrayInitParser.ArrayInitParser(tokens);
-parser.buildParseTrees = true;
+
+var VerboseListener = function() {};
+VerboseListener.prototype = Object.create(Error.ErrorListener.prototype);
+
+
+VerboseListener.prototype.syntaxError = function (recognizer, offendingSymbol, line, charPositionInLine, msg, e) {
+
+    console.log("Offending Symbol: " + offendingSymbol);
+    console.log(" charPositionInLine " + charPositionInLine);
+    console.log("line " + line);
+    console.log("Msg: " + msg);
+};
+
+debugger;
+parser.removeErrorListeners();
+parser.addErrorListener(new VerboseListener());
+
 var tree = parser.init();
 
-var ShortToUnicodeString = function () {
-
-    this.hex = "";
-};
-
-ShortToUnicodeString.prototype = Object.create(ArrayInitListener.ArrayInitListener.prototype);
-
-ShortToUnicodeString.prototype.enterInit = function (ctx) {
-
-    this.hex += "\"";
-};
-
-ShortToUnicodeString.prototype.exitInit = function (ctx) {
-
-    this.hex += "\"";
-};
-
-ShortToUnicodeString.prototype.enterValue = function (val) {
-
-    var hexVal = "\\u0000";
-    var hexUnformatted = (val.INT().getText() * 1).toString(16);
-
-    this.hex += hexVal.substring(0, hexVal.length - hexUnformatted.length) + hexUnformatted;
-};
-
-var ref = new ShortToUnicodeString();
-new Tree.ParseTreeWalker().walk(ref, tree);
-
-console.log(ref.hex);
-
+console.log("Done Done Done");
